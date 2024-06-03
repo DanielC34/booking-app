1e0203446201574e706a50ee4cd36ed8fb0eeac2
Daniel Chanda <danielchanda41@gmail.com>
2024-06-03 17:55:24 +0300

    Booking app ui setup

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,19 @@
 import "./navbar.css"
+import { Button } from "@mui/material";
+import Box from "@mui/material/Box";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
-        <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton">Login</button>
-        </div>
+        <span className="logo">Bookify</span>
+        <Box className="navItems" sx={{ display: "flex", gap: 2 }}>
+          <Button variant="contained">Signup</Button>
+          <Button variant="contained">Login</Button>
+        </Box>
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
